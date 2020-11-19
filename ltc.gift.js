490a80aec480ff3c8363012112c2b9b5c2c1c4d9
gedabuz <30450582+gedabuz@users.noreply.github.com>
2020-11-20 01:43:41 +0700

    Update ltc.gift.js

--- ltc.gift.js
+++ ltc.gift.js
@@ -4,6 +4,8 @@
 // @version      0.1
 // @description  Optimasi Claiming
 // @author       Gedabuz
+// @updateURL https://raw.githubusercontent.com/reek/anti-adblock-killer/master/anti-adblock-killer.user.js
+// @downloadURL https://raw.githubusercontent.com/reek/anti-adblock-killer/master/anti-adblock-killer.user.js
 // @include      https://jejakatutorial.com/bot/LTC/claim
 // ==/UserScript==
 
