d910ad6bce4c1d8148cf78a3d645851fc1115949
gedabuz <30450582+gedabuz@users.noreply.github.com>
2020-11-20 01:20:05 +0700

    Create Ltcgif.js

---
+++ Ltcgif.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         LTCgift
+// @namespace    claim
+// @version      0.1
+// @description  Optimasi Claiming
+// @author       Gedabuz
+// @include      https://jejakatutorial.com/bot/LTC/claim
+// ==/UserScript==
+
+document.getElementsByClassName("btn-primary")[0].click()
+document.getElementsByClassName("btn-primary")[2].click()
